a88404ce9e4a42539b769a5b6b71f7dbaac04ad2
Rebrithed-Reaperr <85047722+Rebrithed-Reaperr@users.noreply.github.com>
2021-05-30 01:05:08 -0500

    Add files via upload

--- framework.py
+++ framework.py
@@ -4,3 +4,27 @@ from os import system, name
 from time import sleep
 
 X = str(input("type 99 to start:"))
+
+if '99' or '66' in X:
+    os.system(' clear ')
+     print("                       WHO ARE YOU?                  ")
+       print("                      :>3qKKKKKKKq3>:                ")
+       print("                  ';CpKKKKKKKKKKKKKKKpC;'            ")
+       print("              -iPKKKKKKKKKKKKKKKKKKKKKKKPi-          ")
+       print("          `~v]KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK]v~`    ")
+       print("       ,rwKKKKKKKKKKKKKPv;,:'-':,;vPKKKKKKKKKKKKKwr, ")
+       print("      !KKKKKKKKKKKKKKK/             !KKKKKKKKKKKKKKK!")
+       print("      !KKKKKKKKKKKKKKf               CKKKKKKKKKKKKKK!")
+       print("      !KKKKKKKKKKKKKp-               -qKKKKKKKKKKKKK!")
+       print("      !KKKKKKKKKKKKK>"               "\KKKKKKKKKKKKK!")
+       print("      !KKKKKKKw;,_'-                   .-:,wKKKKKKK! ")
+       print("      !KKKKKKKKhi*                      KKKKKKKKKKKK ")
+       print("      !KKKKKKKKKKKKK;                 ;KKKKKKKKKKKKK!")
+       print("      !KKKKKKKKKKKKK2>'             '>2KKKKKKKKKKKKK!")
+       print("      !KKKKKKKKKKKKKKKZ             ZKKKKKKKKKKKKKKK!")
+       print("      !KKKKKKKKKKKKKKK5             eKKKKKKKKKKKKKKK!")
+       print("      !KKKKKKKKKKKqC;-               -;CqKKKKKKKKKKK!")
+       print("      <KKKKKKKKkr,                       ,rSKKKKKKKK<")
+       print("       -v]qj;-                             -;jq]v-   ")
+       time.sleep(1.0)
+       os.system(' clear ')
